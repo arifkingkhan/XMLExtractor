05a993eeac8d7c23c15ce18322d5a8a1790af855
arifkingkhan <arif.king.khan@gmail.com>
2020-11-19 15:08:27 +0530

    first commit

---
+++ XML EXTRACTOR/XML-EXTRACTOR/src/main/java/com/XML/EXTRACTOR/XmlExtractorApplication.java
@@ -0,0 +1,13 @@
+package com.XML.EXTRACTOR;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlExtractorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XmlExtractorApplication.class, args);
+	}
+
+}
---
+++ XML EXTRACTOR/XML-EXTRACTOR/src/test/java/com/XML/EXTRACTOR/XmlExtractorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.XML.EXTRACTOR;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlExtractorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
